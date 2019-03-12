69dd72a2f4c83e1a207d3ba1f66f2bc7472537e6
kacho39 <kachunho1995@gmail.com>
2019-03-12 16:16:04 +0800

    added the tax_calculation page

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const path = require('path');
+
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/tax-calculation', function(req, res){
+  // res.render('tax_calculation.html');
+  // res.sendFile('tax_calculation.html');
+  res.sendFile(path.join(__dirname+'/views/pages/tax_calculation.html'));
+});
 
 app.listen(port, () => console.log(`tax app listening on port: ${port}!`))
