98206e46bc6ef7c67e67788b927c43f47c3382d5
cestbon258 <qijiang7-c@my.cityu.edu.hk>
2019-03-12 16:18:16 +0800

    install ejs

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 7000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', function (req, res) {
+    res.send('views/pages/index.html');
+});
+
 
 app.listen(port, () => console.log(`tax app listening on port: ${port}!`))
