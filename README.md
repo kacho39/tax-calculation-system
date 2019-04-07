41be819f4c371a4ff97f6ee7f31a2f3a7e5a3229
kacho39 <kachunho1995@gmail.com>
2019-04-07 23:16:53 +0800

    final one

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
-# Installation
-Before executing the system, you are required to go through some basic installations processes. Please refer to the following official documents for more details.  
-1. npm: https://www.npmjs.com/get-npm
-2. Node.JS: https://nodejs.org/en/
+# express-ejs
+init
--- main.js
+++ main.js
@@ -2,16 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 const path = require('path');
-let ejs = require('ejs')
-
-app.use(express.static(__dirname + '/public'));
-
-// set the view engine to ejs
-app.set('view engine', 'ejs');
 
 
 app.get('/', function (req, res) {
-  res.render('pages/indexx');
+  // res.render('tax_calculation.html');
+  // res.sendFile('tax_calculation.html');
+  res.sendFile(path.join(__dirname+'/views/pages/tax_calculation.html'));
 });
 
-app.listen(port, () => console.log(`App listening on port: ${port}!`));
+app.listen(port, () => console.log(`tax app listening on port: ${port}!`));
